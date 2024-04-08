7d68cf252539780f78fcd81de6626381754b1c5a
ackimixs <ackimixs@protonmail.com>
2024-04-08 21:25:46 +0200

    SLEEEEP

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -178,11 +178,12 @@ void TCPServer::handleMessage(const std::string& message, int clientSocket)
             int y = static_cast<int>(std::stof(arucoArgs[2]));
             std::string toSend = "strat;arduino;go;" + std::to_string(x) + "," + std::to_string(y);
             this->broadcastMessage(toSend.c_str());
-            usleep(10'000'000);
+            usleep(3'000'000);
             this->broadcastMessage("strat;servo_moteur;fermer pince;1\n");
             this->broadcastMessage("strat;servo_moteur;lever bras;1\n");
+            usleep(1'000'000);
             this->broadcastMessage("strat;arduino;go;1000,1500\n");
-            usleep(10'000'000);
+            usleep(3'000'000);
             this->broadcastMessage("strat;servo_moteur;baisser bras;1\n");
             this->broadcastMessage("strat;servo_moteur;ouvrir pince;1\n");
             this->broadcastMessage("strat;arduino;go;500,500\n");
