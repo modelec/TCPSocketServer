3fd21853ee7c40deb5372238bab5e61bc399ccac
ackimixs <ackimixs@protonmail.com>
2024-04-11 14:09:50 +0200

    speed

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -277,7 +277,7 @@ void TCPServer::startGame() {
     this->broadcastMessage("strat;servo_moteur;fermer pince;1\n");
     this->broadcastMessage("strat;servo_moteur;fermer pince;2\n");
     this->broadcastMessage("strat;servo_moteur;ouvrir pince;0\n");
-    this->broadcastMessage("strat;arduino;speed;120\n");
+    this->broadcastMessage("strat;arduino;speed;150\n");
 
     waitForAruco = true;
     this->broadcastMessage("strat;aruco;get aruco;1\n");
