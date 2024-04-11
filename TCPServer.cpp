8c69b892c15ea183ddb666f360846d38cce7b1a5
ackimixs <ackimixs@protonmail.com>
2024-04-11 18:57:18 +0200

    log every message

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -129,6 +129,8 @@ void TCPServer::acceptConnections()
 
 void TCPServer::handleMessage(const std::string& message, int clientSocket)
 {
+    std::cout << message << " | " << clientSocket << std::endl;
+
     std::vector<std::string> tokens = split(message, ";");
 
     if (tokens.size() != 4)
