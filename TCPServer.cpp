2f505e8ad8614c29496ad1b91745670db3bbef99
ackimixs <ackimixs@protonmail.com>
2024-04-16 11:23:49 +0200

    GO

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -501,7 +501,7 @@ void TCPServer::goToAruco(const ArucoTag &arucoTag, const int pince) {
 
     this->broadcastMessage(toSend);
 
-    double xPrime = arucoTag.pos().first[0] + 5/* - 5*/;
+    double xPrime = arucoTag.pos().first[0] + 30/* - 5*/;
     double yPrime = arucoTag.pos().first[1] - decalage;
 
     double thetaPrime = std::atan2(yPrime, xPrime);
