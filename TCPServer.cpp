2cf9e16c79b79284edf5f3da880e7eefba5106ff
ackimixs <ackimixs@protonmail.com>
2024-04-16 17:57:19 +0200

    speed

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -242,7 +242,7 @@ void TCPServer::handleMessage(const std::string& message, int clientSocket)
         }
     }
     else if (tokens[0] == "arduino" && tokens[2] == "set state") {
-        if (TCPUtils::startWith(tokens[3], "1")) {
+        if (TCPUtils::startWith(tokens[3], "0")) {
             this->isRobotMoving++;
         }
     }
