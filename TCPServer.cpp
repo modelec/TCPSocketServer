ea69dc217cd0f2228c34efd1fd2720345b18677c
ackimixs <ackimixs@protonmail.com>
2024-04-30 16:57:57 +0200

    test

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -670,7 +670,7 @@ void TCPServer::goToAruco(const ArucoTag &arucoTag, const int pince) {
     double yPrime = arucoTag.pos()[1];
     double roll = arucoTag.rot()[1];
 
-    double test = (xPrime - 500) * 0.1;
+    double test = (xPrime - 400) * 0.1;
 
     auto centerPlantX = (20 * std::cos(roll)) + xPrime + test;
     auto centerPlantY = (-20 * std::sin(roll)) + yPrime + decalage;
